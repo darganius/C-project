721e7dc92cf1ceed6b5d37455d68f8c484cfb513
darganius <55693220+darganius@users.noreply.github.com>
2019-11-09 08:28:49 +0600

    asdasd

---
+++ project/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project
+{
+    public class Class1
+    {
+        public struct Student
+        {
+            public string firstName;
+            public string lastName;
+            public string id;
+            public string course;
+            public string specialization;
+            public string score;
+            public string status;
+
+
+
+            public void InfoStud()
+            {
+                Console.Write("Имя студента:");
+                firstName = Console.ReadLine();
+                Console.Write("Фамилия студента:");
+                lastName = Console.ReadLine();
+                Console.Write("ID студента:");
+                id = Console.ReadLine();
+                Console.Write("На каком курсе учится студент:");
+                course = Console.ReadLine();
+                Console.Write("Специализация студента:");
+                specialization = Console.ReadLine();
+                Console.Write("Введите средний балл студента:");
+                int score = Convert.ToInt32(Console.ReadLine());
+                if (score < 70 && score > 50)
+                {
+                    status = "У этого студента нет стипендии, но и нет лето.";
+                }
+                else if (score > 70 && score < 100)
+                {
+                    status = "У этого студента есть стипендия, все хорошо";
+                }
+                else if (score < 50 && score > 25)
+                {
+                    status = "Этот студент остается на пересдачу.";
+                }
+                else if (score < 25)
+                {
+                    status = "Этот студент остается на лето, без пересдачи.";
+                }
+
+            }
+        }
+    }
+}
---
+++ project/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static project.Class1;
+
+
+namespace ConsoleApp4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\Новая папка (2)\\output.txt";
+
+            int count;
+            do
+            {
+                Console.Write("Сколько студентов хотите ввести в систему?:");
+            }
+            while (!int.TryParse(Console.ReadLine(), out count) || count < 1);
+            var student = new Student[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int k;
+                do
+                {
+                    Console.WriteLine("1-Ввести данные о {0}, студенте", i + 1);
+                    Console.WriteLine("2-Выйти");
+                }
+                while (!int.TryParse(Console.ReadLine(), out k));
+                {
+                    switch (k)
+                    {
+                        case 1: student[i].InfoStud(); break;
+                        case 2: return;
+                    }
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                List<string> studlist = new List<string>();
+                for (int x = 0; x < 1; x++)
+                {
+                    studlist.Add(student[i].firstName);
+                    studlist.Add(student[i].lastName);
+                    studlist.Add(student[i].id);
+                    studlist.Add(student[i].course);
+                    studlist.Add(student[i].specialization);
+                    studlist.Add(student[i].score);
+                    studlist.Add(student[i].status);
+
+                }
+                Console.WriteLine("[{0}]", string.Join("  ", studlist));
+                Console.WriteLine("-------------------------------------------------------------------------------------------");
+                string rezult = "";
+                foreach (var s in studlist)
+                    rezult += s + " ";
+                File.AppendAllText(path, rezult + Environment.NewLine);
+            }
+            Console.ReadKey();
+        }
+    }
+}
